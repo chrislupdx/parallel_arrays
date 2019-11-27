a8b0ca2af25b18a67816072f5acac53c6dcc1bdd
Dreamer <christopherlu.pdx@gmail.com>
2019-11-26 16:50:40 -0800

    init

---
+++ labH.cpp
@@ -0,0 +1,24 @@
+//Parellell Arrays
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void loadData(); //reads from a text file and stores the data in par. arrays
+void findLow(); //finds and returns the low temp+corresponding month
+void findHigh(); //finds+returns the high temp+corresponding month
+
+int main()
+{
+const int DAYS_IN_MONTH = 30;
+
+// 2 par arays to store the high and low temps for each month of the year
+double allTemp[2][DAYS_IN_MONTH];
+
+//1 par array to store the names of the 12 months
+//file io with a temps.txt 
+
+
+return 0;
+}
