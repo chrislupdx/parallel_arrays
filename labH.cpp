88dc45de57950dce80e4e605f2f67761ee550294
Dreamer <christopherlu.pdx@gmail.com>
2019-11-26 19:08:22 -0800

    file input established

--- labH.cpp
+++ labH.cpp
@@ -1,24 +1,49 @@
 //Parellell Arrays
-
+#include <fstream>
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
 
-void loadData(); //reads from a text file and stores the data in par. arrays
-void findLow(); //finds and returns the low temp+corresponding month
-void findHigh(); //finds+returns the high temp+corresponding month
+int loadData(ifstream &infile, string months[], int lows[], int highs[], int &rows);
+ //reads from a text file and stores the data in par. arrays
+
+void findLow(int lows[], int rows, int &lowtemp, int &month); 
+//finds and returns the low temp+corresponding month
+
+void findHigh(int highs[], int rows, int &hightemp, int &month);
+ //finds+returns the high temp+corresponding month
 
 int main()
 {
+ifstream inFile;
+
 const int DAYS_IN_MONTH = 30;
+const int UP_AND_DOWN = 2;
 
-// 2 par arays to store the high and low temps for each month of the year
-double allTemp[2][DAYS_IN_MONTH];
+double allTemp[UP_AND_DOWN][DAYS_IN_MONTH];
+string months[12];
+int lows[30];
+int highs[30];
+int rows;
 
-//1 par array to store the names of the 12 months
 //file io with a temps.txt 
+loadData(inFile, months, lows, highs, rows );
 
 
 return 0;
+}
+
+int loadData(ifstream &inFile, string months[], int lows[], int highs[], int &rows )
+{
+string query;
+
+//prompts the user for a location -> takes it
+cout << "input a file location" << endl;
+cin >> query;
+
+inFile.open(query);
+
+
+return 1;
 }
