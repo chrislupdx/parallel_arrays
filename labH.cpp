54d7e3f7e7853f258b1324b641e4d2107aea8b24
Dreamer <christopherlu.pdx@gmail.com>
2019-11-26 20:07:00 -0800

    stream sort of working

--- labH.cpp
+++ labH.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
@@ -22,13 +23,14 @@ const int DAYS_IN_MONTH = 30;
 const int UP_AND_DOWN = 2;
 
 double allTemp[UP_AND_DOWN][DAYS_IN_MONTH];
-string months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+//string months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+string months[12];
 int lows[30];
 int highs[30];
 int rows;
 
 //file io with a temps.txt 
-loadData(inFile, months, lows, highs, rows );
+loadData(inFile, months, lows, highs, rows);
 
 
 return 0;
@@ -37,12 +39,27 @@ return 0;
 int loadData(ifstream &inFile, string months[], int lows[], int highs[], int &rows )
 {
 string query;
+string recieved;
 
-//prompts the user for a location -> takes it
-cout << "input a file location" << endl;
-cin >> query;
+//cout << "input a file location" << endl;
+//cin >> query;
+query = "temps.txt";
+cout << "loaded" << endl;
 
 inFile.open(query);
+
+//while the filestream is being put into a string
+//so w
+
+inFile >> recieved;
+//while (inFile >> recieved)
+//{
+  cout << recieved;
+//}
+
+//can take singular int/string as value I'm trying to figure out how to run 
+//the the plan is to take one thing then split it up
+cout << recieved;
 
 
 return 1;
