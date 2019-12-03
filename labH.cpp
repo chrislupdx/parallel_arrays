b2241953d5f4b25ff9d38c9cf8b0e842af2e5a58
Dreamer <christopherlu.pdx@gmail.com>
2019-12-02 18:52:44 -0800

    rows now drawing off of an actual count in loaddata

--- labH.cpp
+++ labH.cpp
@@ -7,13 +7,10 @@
 using namespace std;
 
 int loadData(ifstream &infile, string months[], int lows[], int highs[], int &rows);
- //reads from a text file and stores the data in par. arrays
 
 void findLow(int lows[], int rows, int &lowtemp, int &month);
-//finds and returns the low temp+corresponding month
 
 void findHigh(int highs[], int rows, int &hightemp, int &month);
- //finds+returns the high temp+corresponding month
 
 int main()
 {
@@ -26,18 +23,22 @@ double allTemp[UP_AND_DOWN][DAYS_IN_MONTH];
 string months[12];
 int lows[30];
 int highs[30];
-int rows = 12; //LOL COULD WE HARD CODE THAT
+int rows; //LOL COULD WE HARD CODE THAT
 int month;
 int lowtemp;
 int hightemp;
 cout << "this program takes a file of month, high, and low temp data and" << endl;
 loadData(inFile, months, lows, highs, rows);
 
+if (!(rows <= 1)) //we need to figure out a way to rig loaddata to modify rows
+{
 findLow(lows, rows, lowtemp, month); //if theres only one row don't run both high+low
 cout << "Lowest temp month is: " << month << " the temp is " << lowtemp << endl;
 
 findHigh(highs, rows, hightemp, month);
 cout << "Highest temp month is: " << month << " the temp is " << hightemp << endl;
+}
+
 
 return 0;
 }
@@ -46,6 +47,7 @@ return 0;
 //fine the higest temp and respective month
 void findHigh(int highs[], int rows, int &hightemp, int &month)
 {
+  cout << "findHigh called" << endl;
   int highest = highs[0];
   for (int i = 0; i < rows; i++)
   {
@@ -61,6 +63,7 @@ void findHigh(int highs[], int rows, int &hightemp, int &month)
 //find the lowest temp and resspective month
 void findLow(int lows[], int rows, int &lowtemp, int &month)
 {
+  cout << "findLow called" << endl;
   int lowest = lows[0];
   for (int i = 0; i < rows; i++) //iterate through and compare for the lowest value
   {
@@ -108,7 +111,7 @@ while (inFile >> recieved)
   }
 } //still needs code to not read more rows than the # of slots in arrays
   cout << "number of months in the file: " << monthsIt << endl;
-
+  rows = monthsIt; //number of months is literally the number of rows
 for (int i = 0; i < numsIt; i++) //work with numbers to get high and low
 {
 
@@ -125,5 +128,5 @@ if (!(i % 2 == 0))
 }
 inFile.close();
 
-return 1;
+return 0;
 }
